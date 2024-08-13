9bafe1fad278831b7513144cdc4c7cf966eefd5e
frederickababio <abafred12@gmail.com>
2024-08-13 15:25:41 +0000

    Numeric Data

--- numeric-data.py
+++ numeric-data.py
@@ -19,16 +19,4 @@ myValue=False
 print(myValue)
 print(type(myValue))
 print(str(myValue) + " is of the data type " + str(type(myValue)))
-myString = "This is a string."
-print(myString)
-print(type(myString))
-print(myString + " is of the data type " + str(type(myString)))
-firstString = "water"
-secondString = "fall"
-thirdString = firstString + secondString
-print(thirdString)
-name = input("What is your name? ")
-print(name)
-color = input("What is your favorite color?  ")
-animal = input("What is your favorite animal?  ")
-print("{}, you like a {} {}!".format(name,color,animal))
+
